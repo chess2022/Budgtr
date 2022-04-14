6b60acae23796beb4ad6e3abec5ef987e8e01942
chess2022 <lakeshore.hnh@gmail.com>
2022-04-13 23:50:46 -0700

    Adds a function to change background
    does not work (YET) lol

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const budget = require("./models/budget.js");
-let bankAccount = 0;
-
 
 
 // Declare middleware
@@ -23,7 +21,14 @@ app.get("/budget/", (req, res) => {
     let bankAccount = budget.reduce((total, item) => {
       return parseInt(total) + parseInt(item.amount);
     }, 0);
-  res.render("index.ejs", {budgetAll: budget, bankAccount});
+    (function () { 
+    let document = "public/index.ejs"
+        if (bankAccount< 0) {
+            document.getElementByClass("total").style.backgroundColor = "red";
+        } else if (bankAccount>1000){
+            document.getElementByClass("total").style.backgroundColor = "green";
+        }});
+        res.render("index.ejs", {budgetAll: budget, bankAccount});
 });
 
 // New route
